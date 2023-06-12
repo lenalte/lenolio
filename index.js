e16fdab19905facaca398a9f1e37391b314af1b7
Lena Ertl <lertl@hm.edu>
2023-06-12 17:36:28 +0200

    images and text ease out when scrolling

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const track = document.getElementById("image-track");
 
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
-
 }
 
 window.onmousemove = e => {
@@ -11,15 +10,12 @@ window.onmousemove = e => {
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
     const maxDelta = window.innerWidth / 2;
 
-    const percentage = (mouseDelta / maxDelta) * -100
-    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    const percentage = (mouseDelta / maxDelta) * -100;
+    let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
     nextPercentage = Math.min(nextPercentage, 0);
     nextPercentage = Math.max(nextPercentage, -100);
 
-
-
     // der Text soll verschwinden wenn man die Bilder verschiebt:
-
     const textElement = document.getElementsByClassName("text")[0];
 
     if (nextPercentage < 0) {
@@ -27,8 +23,6 @@ window.onmousemove = e => {
     } else {
         textElement.classList.remove("hidden");
     }
-
-
 
     track.dataset.percentage = nextPercentage;
 
@@ -48,11 +42,8 @@ window.onmouseup = e => {
     track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-
-
 window.onscroll = e => {
     const textElement1 = document.getElementById("text1");
-    const textElement2 = document.getElementById("text2");
     const frameElement = document.getElementById("frame");
     const imageTrack = document.getElementById("image-track");
 
@@ -61,29 +52,37 @@ window.onscroll = e => {
 
     // Change top property
     textElement1.style.top = `${newTop}px`;
-    textElement2.style.top = `${newTop}px`;
 
     // Check if we scrolled past the initial screen height
     if (window.scrollY > window.innerHeight) {
-        // Hide the first text and show the second
+        // Hide the text
         textElement1.classList.add("hidden");
-        textElement2.classList.remove("hidden");
+        imageTrack.classList.add("hidden");
     } else {
-        // Show the first text and hide the second
+        // Show the text
         textElement1.classList.remove("hidden");
-        textElement2.classList.add("hidden");
+        imageTrack.classList.remove("hidden");
     }
+
+    if (window.scrollY > imageTrack.offsetHeight) {
+        // Hide the images
+        textElement1.classList.add("hidden");
+        imageTrack.classList.add("hidden");
+    } else {
+        // Show the images
+        textElement1.classList.remove("hidden");
+        imageTrack.classList.remove("hidden");
+    }
+
 
     if (window.scrollY > window.innerHeight + imageTrack.offsetHeight) {
         let newImageTrackTop = window.innerHeight + imageTrack.offsetHeight - window.scrollY;
         imageTrack.style.top = `${newImageTrackTop}px`;
         textElement1.style.top = `${newTop - window.scrollY}px`;
-        textElement2.style.top = `${newTop - window.scrollY}px`;
         frameElement.style.marginTop = `${newImageTrackTop}px`;
     } else {
         imageTrack.style.top = "50%";
         frameElement.style.marginTop = "0px";
     }
 }
-
 
